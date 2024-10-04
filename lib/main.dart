dc8d2d4acf526a1a36cc8cf07c66aa8783a88d25
hsshin <hsshin@euclidsoft.co.kr>
2024-10-04 18:01:27 +0900

    test commit 20241004 shs

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Colors.blue,
-      body: Image.asset('asset/img/logo.png'),
+      backgroundColor: Color(0xFF17152e),
+      body: Column(
+        children: [
+          Image.asset('asset/img/logo.png'),
+          Center(
+            child: Text(
+              'Hash Factory',
+              style: TextStyle(color: Color(0xFF03ff0f)),
+            ),
+          ),
+        ],
+      ),
     ),
   ));
 }
