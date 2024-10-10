3a92f5fd6928fc4ae43f9962208bcbd3434b2955
hsshin <hsshin@euclidsoft.co.kr>
2024-10-10 09:06:24 +0900

    finish commit 20241010 shs

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Color(0xFF17152e),
-      body: Column(
-        children: [
-          Image.asset('asset/img/logo.png'),
-          Center(
-            child: Text(
+  runApp(
+    const MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
+
+/// StatelessWidget
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF17152e),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('asset/img/logo.png'),
+            const Text(
               'Hash Factory',
               style: TextStyle(color: Color(0xFF03ff0f)),
             ),
-          ),
-        ],
+            const SizedBox(height: 50.0),
+            const CircularProgressIndicator(color: Colors.white),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("my ", style: TextStyle(color: Color(0xFF03ff0f)),),
+                Text("name ", style: TextStyle(color: Color(0xFF03ff0f)),),
+                Text("is ", style: TextStyle(color: Color(0xFF03ff0f)),),
+                Text("hash ", style: TextStyle(color: Color(0xFF03ff0f)),),
+              ],
+            )
+          ],
+        ),
       ),
-    ),
-  ));
+    );
+  }
 }
